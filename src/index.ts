05b645dc0ff40a3761675497eafc350dbd08e94a
Nikhil Pathak <nikhilpathak2602@gmail.com>
2025-09-16 11:29:54 +0530

    Initializing Socket.io

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,32 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import UserRouter from './Routes/UserRoutes';
 import passport from 'passport';
+import { Server } from 'socket.io';
+import http from 'http';
+
+
+
+const corsWithOptions = {
+    origin: "http://localhost:5173",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}
+
 
 const app: any = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        credentials: true,
+    }
+});
+
+io.on("connection", (socket: any) => {
+    console.log("new connection",socket.id);
+    socket.emit("welcome","Welcome to the server");
+});
 
 
 const Mongo_url: any = process.env.MONGO_URL;
@@ -33,6 +57,10 @@ if (Secret_key) {
     }));
 }
 
+
+
+
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, '../public')));
@@ -40,7 +68,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json())
 app.use(passport.initialize());
-app.use(cors());
+app.use(cors(corsWithOptions));
 
 
 app.use("/api/users", UserRouter);
@@ -50,6 +78,6 @@ app.get("/", (req: Request, res: Response) => {
 })
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log("Server Running at ", port)
 })
